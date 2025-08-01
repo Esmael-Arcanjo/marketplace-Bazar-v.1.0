b9e4022da190dc7f5e4948bce0480446bb972dee
Esmael Arcanjo <leamseojnacra@gmail.com>
2025-08-01 17:54:52 -0300

    marketplace-Bazar-v.1.0

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Esmael Arcanjo',
+                'username' => 'Leamse',
+                'email' => 'leamse@gmail.com',
+                'role' => 'admin',
+                'status' => 'active',
+                 'password' => bcrypt('password')
+            ],
+
+               [
+                'name' => 'Vendedor Vendor',
+                'username' => 'Vendedor',
+                'email' => 'vendedor@gmail.com',
+                'role' => 'vendor',
+                'status' => 'active',
+                 'password' => bcrypt('password')
+               ],
+
+               [
+                'name' => 'Cliente user',
+                'username' => 'user',
+                'email' => 'Cliente@gmail.com',
+                'role' => 'user',
+                'status' => 'active',
+                 'password' => bcrypt('password')
+            ]
+        ]);
+    }
+}
